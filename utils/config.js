68c86acbcb1b7a39c0f74dbf55ef264d8e9f11ed
Shu'aib Nurudeen Olawale <snotatonline@gmail.com>
2025-03-17 11:29:29 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 console.log('dotenv', require('dotenv').config())
-const PORT = process.env.PORT
+const port = process.env.PORT || 8000
 const MONGODB_URI = process.env.MONGO_URI
 const JWT_SECRET = process.env.JWT_SECRET
 
 module.exports = {
     MONGODB_URI,
-    PORT,
+    port,
     JWT_SECRET
 }
